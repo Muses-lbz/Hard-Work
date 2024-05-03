7c1e9877ba66aae2ffd00f65c62315283d733747
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2024-05-03 17:00:32 +0800

    Create Beginner's intention.cpp

---
+++ Beginner's intention.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int Printf()
+    {
+        cout << "Hello World!" << endl;
+        return 0;
+    }
+private:
+
+protected:
+
+};
+
+int main()
+{
+    Solution S;
+    S.Printf();
+
+    return 0;
+}
